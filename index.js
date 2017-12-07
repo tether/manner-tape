1bbaa4242ef1c71d13469347b7324b1779f117e9
bredele <olivier.wietrich@gmail.com>
2017-12-07 13:37:13 -0700

    Dynamically test method based on results

--- index.js
+++ index.js
@@ -20,25 +20,42 @@ module.exports = service
 
 function service(service, json) {
   const test = manner(service)
-  Object
-    .keys(json)
-    .map(key => {
-      const method = json[key]
-      Object.keys(method).map(path => {
-        method[path].map(obj => {
-          const result = obj.result
-          tape(obj.description, assert => {
-            assert.plan(2)
-            test[key](path, obj.query || {}, obj.body || {}).then(response => {
-              assert.equal(response.status, result.status)
-              assert.deepEqual(response.payload, result.payload)
-            }, err => {
-              console.log('ERROR', err)
-            })
+  Object.keys(json).map(key => {
+    const method = json[key]
+    Object.keys(method).map(path => {
+      method[path].map(obj => {
+        const result = obj.result
+        const {
+          status,
+          payload
+        } = result
+        tape(obj.description, assert => {
+          assert.plan(plan(status, payload))
+          test[key](path, obj.query || {}, obj.body || {}).then(response => {
+            if (status != null) assert.equal(response.status, status)
+            if (payload != null) assert.deepEqual(response.payload, payload)
           })
         })
       })
     })
+  })
+}
+
+
+/**
+ * Plan the number or tests based
+ * on the number of arguments.
+ *
+ * @return {Number}
+ * @api private
+ */
+
+function plan () {
+  var nb = 0
+  for (var l = arguments.length; l--;) {
+    if (arguments[l] != null) nb++
+  }
+  return nb
 }
 
 service.test = tape
