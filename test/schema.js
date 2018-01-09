572bf47e1e894804bd2699f2469d3a7cb97e07b5
Alexander Bird <alex.bird@arcurve.com>
2018-01-09 13:59:30 -0700

    Remove stray console.log('YOUHOUUUUU') in test/schema.js

--- test/schema.js
+++ test/schema.js
@@ -4,7 +4,6 @@ module.exports = {
         query: {
           name: {
             validate(value) {
-              console.log('YOUHOUUUUU')
               return !(value === 'ellon')
             }
           }
